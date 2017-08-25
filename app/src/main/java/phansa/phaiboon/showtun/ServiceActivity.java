28d3ca22c3f4d321e717f44257fe51c726accdb7
MasterUNG <phrombutr@gmail.com>
2017-08-25 15:58:49 +0700

    finish 25Aug

--- app/src/main/java/phansa/phaiboon/showtun/ServiceActivity.java
+++ app/src/main/java/phansa/phaiboon/showtun/ServiceActivity.java
@@ -1,16 +1,32 @@
 package phansa.phaiboon.showtun;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import phansa.phaiboon.showtun.fragment.ReadEbookFragment;
 
 public class ServiceActivity extends AppCompatActivity {
 
+    //Explicit
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+        //Initial View
+        initialView();
+
 
         //Add Fragment
         if (savedInstanceState == null) {
@@ -22,4 +38,56 @@ public class ServiceActivity extends AppCompatActivity {
 
     }   // onCreate
 
+    private void initialView() {
+
+        //Setup ToolBar
+        toolbar = (Toolbar) findViewById(R.id.toolBarService);
+        setSupportActionBar(toolbar);
+
+        //Setup Drawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerMenu);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                ServiceActivity.this,
+                drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+        );
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }   // initialView
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }   // Main Class
+
+
+
+
+
+
+
+
+
