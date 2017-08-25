b1c0eb01c9a0c0a9f24a4fb976401a68ee67c8dc
MasterUNG <phrombutr@gmail.com>
2017-08-25 11:56:19 +0700

    Before Lunch

--- app/src/main/java/phansa/phaiboon/showtun/fragment/MainFragment.java
+++ app/src/main/java/phansa/phaiboon/showtun/fragment/MainFragment.java
@@ -3,18 +3,26 @@ package phansa.phaiboon.showtun.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import phansa.phaiboon.showtun.R;
+import phansa.phaiboon.showtun.manager.MyAlert;
 
 /**
  * Created by masterung on 8/23/2017 AD.
  */
 
 public class MainFragment extends Fragment{
+
+    //Explicit
+    private String userString, passwordString;
+
 
     @Nullable
     @Override
@@ -32,7 +40,50 @@ public class MainFragment extends Fragment{
         //NewRegister Controller
         newRegisterController();
 
+        //Login Controller
+        loginController();
+
     }   // onActivityCreated
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Initial View
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                //Get Value From EditText
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.myDialog(getResources().getString(R.string.title_have_space),
+                            getResources().getString(R.string.message_have_space));
+                } else {
+                    //No Space
+                    checkUserAndPass();
+                }
+
+            }   // onClick
+        });
+    }
+
+    private void checkUserAndPass() {
+
+        String tag = "25AugV1";
+
+        try {
+
+        } catch (Exception e) {
+            Log.d(tag, "e checkUser ==> " + e.toString());
+        }
+
+    }   // checkUserAndPass
 
     private void newRegisterController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
