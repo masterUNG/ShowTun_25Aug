0cd804e32bc1507c08af227de4c63013b9c469f4
MasterUNG <phrombutr@gmail.com>
2017-08-28 11:30:41 +0700

    Read JSON for ebookTABLE OK

--- app/src/main/java/phansa/phaiboon/showtun/fragment/ReadEbookFragment.java
+++ app/src/main/java/phansa/phaiboon/showtun/fragment/ReadEbookFragment.java
@@ -3,11 +3,15 @@ package phansa.phaiboon.showtun.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import phansa.phaiboon.showtun.R;
+import phansa.phaiboon.showtun.manager.GetAllData;
+import phansa.phaiboon.showtun.manager.MyConstant;
 
 /**
  * Created by masterung on 8/25/2017 AD.
@@ -17,8 +21,38 @@ public class ReadEbookFragment extends Fragment{
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_read_ebook, container, false);
         return view;
-    }
+    }   // onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Create ListView
+        createListView();
+
+    }   // onActivityCreated
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.livEbook);
+        String tag = "28AugV1";
+        MyConstant myConstant = new MyConstant();
+
+        try {
+
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myConstant.getUrlEbookString());
+            String strJSON = getAllData.get();
+            Log.d(tag, "JSON ==> " + strJSON);
+
+        } catch (Exception e) {
+            Log.d(tag, "e createListView ==> " + e.toString());
+        }
+
+    }   // createListView
+
 }   // Main Class
---
+++ app/src/main/java/phansa/phaiboon/showtun/manager/MyConstant.java
@@ -0,0 +1,14 @@
+package phansa.phaiboon.showtun.manager;
+
+/**
+ * Created by masterung on 8/28/2017 AD.
+ */
+
+public class MyConstant {
+
+    private String urlEbookString = "http://androidthai.in.th/Tun/getAllEbookMaster.php";
+
+    public String getUrlEbookString() {
+        return urlEbookString;
+    }
+}   // Main Class
